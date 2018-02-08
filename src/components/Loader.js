cb106ac49e19218d9a65ad9dc761da1ae4e6afc3
Lionel Adams <adamslionel@hotmail.co.uk>
2018-02-08 11:18:10 +0000

    feat: Add Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import { LOADING_MESSAGE } from '../constants/constants';
+
+class Loader extends Component {
+  render() {
+    return (
+      <div className="loader">
+        <i className="fas fa-spinner fa-4x" />
+        <p>{LOADING_MESSAGE}</p>
+      </div>
+    );
+  }
+}
+
+export default Loader;
