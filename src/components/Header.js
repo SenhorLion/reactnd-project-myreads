dd0f28fd1facc172b25580f93bf464d3ebdfb616
Lionel Adams <adamslionel@hotmail.co.uk>
2018-02-08 11:17:14 +0000

    refactor: Add showLink prop to enable linking back to root

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class Header extends Component {
@@ -7,17 +8,21 @@ class Header extends Component {
   };
   static defaultProps = {
     title: 'My Reads',
+    showLink: false,
   };
 
   render() {
-    const { title } = this.props;
+    const { title, showLink } = this.props;
 
     return (
-      <div className="list-books-title">
-        <header>
-          <h1>{title}</h1>
-        </header>
-      </div>
+      <header className="app-header">
+        {showLink && (
+          <Link className="link-back" to="/">
+            <i className="fas fa-arrow-left fa-2x" />
+          </Link>
+        )}
+        <h1>{title}</h1>
+      </header>
     );
   }
 }
