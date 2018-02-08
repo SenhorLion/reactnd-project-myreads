6bf081c0634687341541f886172c1b1d09354c59
Lionel Adams <adamslionel@hotmail.co.uk>
2018-02-08 11:17:54 +0000

    feat: Add constants to minimise string copy in UI

---
+++ src/constants/constants.js
@@ -0,0 +1,17 @@
+export const BOOK_SHELF_CATEGORIES = {
+  CURRENTLY_READING: 'currentlyReading',
+  WANT_TO_READ: 'wantToRead',
+  READ: 'read',
+};
+
+export const ERRORS = {
+  EMPTY_QUERY: 'empty query',
+  TYPE_ERROR: 'TypeError',
+};
+
+export const ERROR_MESSAGES = {
+  QUERY_NOT_FOUND: 'No results found for your search term',
+};
+
+export const SEARCH_TERM_TITLE = 'Search term:';
+export const LOADING_MESSAGE = 'Loading';
