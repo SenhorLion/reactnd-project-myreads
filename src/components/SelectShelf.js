edd0219c4c566ec6903c7f4fcd6d75cb29983eba
Lionel Adams <adamslionel@hotmail.co.uk>
2018-02-09 15:41:35 +0000

    feat: Add SelectShelf component

---
+++ src/components/SelectShelf.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import { LOADING_MESSAGE } from '../constants/constants';
+
+class SelectShelf extends Component {
+  render() {
+    const { shelf, book, onChange } = this.props;
+
+    return (
+      <select
+        value={shelf}
+        onChange={event => onChange(book, event.target.value)}
+      >
+        <option value="none" disabled>
+          Move to...
+        </option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    );
+  }
+}
+
+export default SelectShelf;
