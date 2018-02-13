77dc8d95f909666d3f06a5b8532ac1a3227b3af8
Lionel Adams <adamslionel@hotmail.co.uk>
2018-02-13 10:33:36 +0000

    refactor: Add overlay to Loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -5,9 +5,11 @@ import { LOADING_MESSAGE } from '../constants/constants';
 class Loader extends Component {
   render() {
     return (
-      <div className="loader">
-        <i className="fas fa-spinner fa-4x" />
-        <p>{LOADING_MESSAGE}</p>
+      <div className="loader-overlay">
+        <div className="loader">
+          <i className="fas fa-spinner fa-4x" />
+          <p>{LOADING_MESSAGE}</p>
+        </div>
       </div>
     );
   }
